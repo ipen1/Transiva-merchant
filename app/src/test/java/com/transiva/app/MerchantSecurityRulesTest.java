e0a89f648d53ec9e178abd31e8a465e3040370e9
ipen1 <68970625+ipen1@users.noreply.github.com>
2026-08-11 07:01:47 +0800

    Add files via upload

--- app/src/test/java/com/transiva/app/MerchantSecurityRulesTest.java
+++ app/src/test/java/com/transiva/app/MerchantSecurityRulesTest.java
@@ -1,1 +1,23 @@
+package com.transiva.app;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MerchantSecurityRulesTest {
+    @Test public void usernamesAreValidated() {
+        assertTrue(MerchantSecurityRules.isUsernameValid("merchant.01"));
+        assertFalse(MerchantSecurityRules.isUsernameValid("AB"));
+        assertFalse(MerchantSecurityRules.isUsernameValid("merchant space"));
+    }
+    @Test public void passwordsNeedLetterNumberAndLength() {
+        assertTrue(MerchantSecurityRules.isStrongPassword("Transiva2026"));
+        assertFalse(MerchantSecurityRules.isStrongPassword("12345678"));
+        assertFalse(MerchantSecurityRules.isStrongPassword("password"));
+        assertFalse(MerchantSecurityRules.isStrongPassword("a1"));
+    }
+    @Test public void pinMustBeExactlySixDigits() {
+        assertTrue(MerchantSecurityRules.isPinValid("123456"));
+        assertFalse(MerchantSecurityRules.isPinValid("12345"));
+        assertFalse(MerchantSecurityRules.isPinValid("12a456"));
+    }
+}
